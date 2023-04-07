c156c7b133a5bda4c511f0c0c7c8a21779e3a8fc
Jan0Fu <115988224+Jan0Fu@users.noreply.github.com>
2023-04-07 21:09:18 +0200

    refactor: optimize repeated code and redundancies

--- src/mealplanner/SaveMode.java
+++ src/mealplanner/SaveMode.java
@@ -7,16 +7,20 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.Scanner;
+import java.util.*;
 
 public class SaveMode {
 
     private final Scanner scan;
     private final Connection connection;
+    private final List<String > listWithDuplicates;
+    private final Map<String, Integer> shoppingList;
 
     public SaveMode(Scanner scan, Connection connection) {
         this.scan = scan;
         this.connection = connection;
+        this.listWithDuplicates = new ArrayList<>();
+        this.shoppingList = new HashMap<>();
     }
 
     public void save() throws SQLException {
@@ -26,19 +30,35 @@ public class SaveMode {
             System.out.println("Input a filename:");
             File file = new File(scan.nextLine());
             try {
-                PrintWriter printWriter = new PrintWriter(file);
+                PrintWriter writer = new PrintWriter(file);
                 do {
                     Statement statement2 = connection.createStatement();
                     int meal_id = rs1.getInt("meal_id");
                     ResultSet rs2 = statement2.executeQuery("select ingredient from ingredients where meal_id=%d".formatted(meal_id));
                     while (rs2.next()) {
-                        printWriter.println(rs2.getString("ingredient"));
+                        String ingredient = rs2.getString("ingredient");
+                        listWithDuplicates.add(ingredient);
                     }
                     statement2.close();
                 }
                 while (rs1.next());
-                printWriter.flush();
-                printWriter.close();
+                for (String key: listWithDuplicates) {
+                    if (shoppingList.containsKey(key)) {
+                        int count = shoppingList.get(key) + 1;
+                        shoppingList.put(key, count);
+                    } else {
+                        shoppingList.put(key, 1);
+                    }
+                }
+                for (String key: shoppingList.keySet()) {
+                    if (shoppingList.get(key) > 1) {
+                        writer.println(key + " x" + shoppingList.get(key));
+                    } else {
+                        writer.println(key);
+                    }
+                }
+                writer.flush();
+                writer.close();
                 statement1.close();
                 System.out.println("Saved!");
             } catch (IOException e) {
